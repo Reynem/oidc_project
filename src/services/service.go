b696310b449edf51f5a036836d488a5592fe4cf2
Admin <lex.meins@proton.me>
2025-12-28 17:57:42 +0500

    init service

--- src/services/service.go
+++ src/services/service.go
@@ -1,1 +1,27 @@
 package services
+
+import (
+	repo "oidc_project/repository"
+)
+
+type MainService struct {
+	repo repo.MainRepository
+}
+
+func (m MainService) WriteData(data string) error {
+	err := m.repo.WriteData(data)
+	return err
+}
+
+func (m MainService) ReadData() (string, error) {
+	data, err := m.repo.ReadData()
+	if err != nil {
+		return "", err
+	}
+	return data, nil
+}
+
+func (m MainService) CleanData() error {
+	err := m.repo.CleanData()
+	return err
+}
