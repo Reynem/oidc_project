a5ee613a1a8180ee92f740451ec55cab7124a9be
Admin <lex.meins@proton.me>
2025-12-29 12:26:45 +0500

    router init

--- src/transport/router.go
+++ src/transport/router.go
@@ -1,1 +1,56 @@
 package transport
+
+import (
+	"encoding/json"
+	"net/http"
+	"oidc_project/dto"
+	"oidc_project/models"
+	"oidc_project/services"
+)
+
+type MainController struct {
+	serv services.MainService
+}
+
+func NewMainController(serv services.MainService) *MainController {
+	return &MainController{serv: serv}
+}
+
+func (m *MainController) GetData(w http.ResponseWriter, r *http.Request) *dto.MainResponse {
+	data, err := m.serv.ReadData()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return &dto.MainResponse{Data: ""}
+	}
+
+	return &dto.MainResponse{Data: data}
+}
+
+func (m *MainController) WriteData(w http.ResponseWriter, r *http.Request) {
+	var data models.BasicRequest
+	err := json.NewDecoder(r.Body).Decode(&data)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = m.serv.WriteData(data.Data)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (m *MainController) CleanData(w http.ResponseWriter, r *http.Request) {
+	err := m.serv.CleanData()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
